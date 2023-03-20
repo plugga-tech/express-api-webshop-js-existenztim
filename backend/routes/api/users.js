8f4628339f06d7f23046f9140730dc8b693235c6
Tim Sundell <tim920129@hotmail.com>
2023-03-20 14:30:27 +0100

    :construction: first version of user endpoints done

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,10 +5,10 @@ const CryptoJS = require('crypto-js');
 
 // HÄMTA ALLA USERS // SKICKA INTE MED LÖSENORD // BARA ID, NAMN + EMAIL PÅ ALLA USERS
 router.get('/', async (req, res) => { 
-  const product = await UserModel.find()
+  const user = await UserModel.find({}, {name:1, email:1})
 
   try {
-  res.status(200).json(product);
+  res.status(200).json(user);
   // res.send("test");
 
   } catch (err){
@@ -18,8 +18,16 @@ router.get('/', async (req, res) => {
 })
 
 // HÄMTA SPECIFIK USER // SKICKA HELA OBJEKTET ska det vara post?
-router.get('/:userId', function (req, res, next) {
-  res.send('Hello from userId Endpoint!');
+router.post('/', async (req, res, next) => {
+  const { id } = req.body;
+  const user = await UserModel.findById({_id: id})
+
+  try{
+    res.status(200).json(user);
+  } catch (err){
+    console.error(err);
+    res.status(500).send("Server Error")
+  }
 });
 
 // SKAPA USER
@@ -39,9 +47,27 @@ router.post('/add', async (req, res, next) => {
 })
 
 // LOGGA IN USER
-router.post('/login', function (req, res, next){
-  res.send('Hello from login Endpoint!');
-})
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+    //decrypt password
+    let decryptPassword = CryptoJS.AES.decrypt(user.password, "salt key").toString(CryptoJS.enc.Utf8);
+    if (password === decryptPassword) {
+      res.status(200).json(user);
+    } else {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
 
 
