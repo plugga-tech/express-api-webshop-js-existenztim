a8647252e83bdd593e1964a6600d3fab24a6fe51
Tim Sundell <tim920129@hotmail.com>
2023-03-23 19:22:26 +0100

    :star: Fixed format for send order to match model

--- frontend/src/script/handleOrder.js
+++ frontend/src/script/handleOrder.js
@@ -3,17 +3,22 @@ const cartIcon = document.querySelector("#cart span");
 let publishedBaseUrl = "http://localhost:3000/api/";
 
 export const sendOrder = async () => {
-    const cartToSend = JSON.parse(localStorage.getItem("productCart"));
-    const userId = parseInt(localStorage.getItem("userid"));
-
+    const cartToFilter = JSON.parse(localStorage.getItem("productCart"));
+    console.log("ofilterad array: ",cartToFilter);
+    const filteredCart = cartToFilter.map(product => ({ productId: product._id, quantity: product.quantity }))
+    const userId = (localStorage.getItem("userid"));
+    console.log(userId);
+    console.log("filterad array: ",filteredCart)
     let userOrder = {
         user: userId, 
-        products: cartToSend
+        products: filteredCart
       }
+    console.log(userOrder)
 
-    if(localStorage.getItem("username")){
+    
+if(localStorage.getItem("username") && filteredCart.length > 0){
         try {
-            const response = await fetch(`${publishedBaseUrl}users/add`,{
+            const response = await fetch(`${publishedBaseUrl}orders/add`,{
                 method:"POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -29,11 +34,14 @@ export const sendOrder = async () => {
                 `;
                 alert("We have recived your order.");
                 cartIcon.innerHTML = 0;
+                localStorage.removeItem("productCart");
             }
         } catch(err) {
             greeting.innerText = err;
         }
+    } else if(filteredCart.length = 0){
+        alert("You have no products in the cart")
     } else {
-        alert("You must be logged in to send an order!")
+        alert("You must be logged in the send an order!")
     }
 }
