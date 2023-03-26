5b17cfd51e1feb6714bbe952d250eae25bfd0daf
Tim Sundell <tim920129@hotmail.com>
2023-03-26 14:55:34 +0200

    :tada: added user details

--- frontend/src/script/myPage.js
+++ frontend/src/script/myPage.js
@@ -1,29 +1,37 @@
 
 let publishedBaseUrl = "http://localhost:3000/api/";
-export const fetchUserDetails = () => {
+export const fetchUserDetails = async () => {
     if(localStorage.getItem("userid")){
         const userId = (localStorage.getItem("userid"));
-    console.log(userId)
-    let userDetails = {
-        id: userId, 
-      }
-      console.log(userDetails);
-    fetch(`${publishedBaseUrl}users`, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(userDetails)
-    })
-    .then(response => response.json())
-    .then(data => {
-    console.log(data);
-    })
-    .catch(err => {
-    console.error(err);
-    });
+        let userDetails = {
+            id: userId, 
+        }
+    try {
+        const response = await fetch(`${publishedBaseUrl}users`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userDetails)
+            })
+        const data = await response.json()
+        generateUserDetails(data)
+    } catch (err) {
+        console.error(err)
+    }
     } else {
         alert("You are not logged in!")
     }
     
+}
+
+const generateUserDetails = (user) => {
+    const userList = document.getElementById("userList");
+    userList.innerHTML = /*html */`
+        <h3>Your details below:<h3>
+        <ul id=${user._id}>
+            <li>username: ${user.name}</li>
+            <li>email: ${user.email}</li>
+        <ul>
+        `
 }
